3ed0856877daa0c2028304abbf933fb994e360b3
Vikrant Satheesh kumar <Vikrant@Vikrant-3.local>
2017-01-21 17:27:09 -0500

    PhantomJS issue not fixed yet

--- camParser.py
+++ camParser.py
@@ -26,6 +26,8 @@ import re
 import sys
 import time
 import json
+from selenium import webdriver
+import platform
 
 def nycdot():
     print ("NYC dot")
@@ -33,10 +35,17 @@ def nycdot():
     JSonURL = "http://dotsignals.org/new-data.php" # JSON File containing MAP data.
     CameraPopupURL = "http://dotsignals.org/google_popup.php?cid=" # Camera URL access.
 
+    if platform.system() == 'Windows':
+        PHANTOMJS_PATH = './phantomjs.exe'
+    else:
+        PHANTOMJS_PATH = './phantomjs'
+
+    browser = webdriver.PhantomJS(PHANTOMJS_PATH)
+
     f = open ('nycdot_list', 'w') # Write to an output file.
 
     #Header Info
-    
+    f.write ("description#snapshot_url#latitude#longitude#country#city\n")
 
     #load JSON file into the response to parse.
     response = urllib2.urlopen(JSonURL).read()
@@ -44,8 +53,24 @@ def nycdot():
     #Parse the given response.
     parsed_json = json.loads(response)
 
+    #get the markers to help parse.
+    cameras = parsed_json ['markers']
 
+    for camera in cameras:
+        cam_id    = camera['id']
+        content   = camera['content']
+        latitude  = camera['latitude']
+        longitude = camera['longitude']
+        url       = CameraPopupURL + cam_id
 
+        browser.get(url)
+        soup = BeautifulSoup(browser.page_source)
+
+        snapshot_url = soup.find('img').get('src')
+        print (snapshot_url)
+        pass
+
+    f.close()
     return
 
 if __name__ == '__main__':
